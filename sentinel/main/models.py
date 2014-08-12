2025a542285d30d0db626d102c0a624d4c028500
Moshi Bin <moshi.bin@gmail.com>
2014-08-12 19:11:41 +0300

    Added basic models

--- sentinel/main/models.py
+++ sentinel/main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Repo(models.Model):
+    name = models.CharField(max_length=60)
+    url = models.CharField(max_length=200)
+
+
+class File(models.Model):
+    path = models.CharField(max_length=300)
+    repo = models.ForeignKey(Repo)
+
+
+class FileScore(models.Model):
+    src_file = models.ForeignKey(File)
+    test_date = models.DateTimeField(auto_now_add=True)
+    score = models.DecimalField()
